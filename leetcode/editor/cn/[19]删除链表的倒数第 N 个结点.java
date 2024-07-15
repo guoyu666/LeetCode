489a30e9fcb6c6e6b96947e1ddd2e3e038dbf8ce
GuoYu <1262425287@qq.com>
2024-07-15 11:35:55 +0800

    提交代码

--- leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.java
+++ leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.java
@@ -1,106 +1,108 @@
 /**
-给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
-
- 
-
- 示例 1： 
- 
- 
-输入：head = [1,2,3,4,5], n = 2
-输出：[1,2,3,5]
- 
-
- 示例 2： 
-
- 
-输入：head = [1], n = 1
-输出：[]
- 
-
- 示例 3： 
-
- 
-输入：head = [1,2], n = 1
-输出：[1]
- 
-
- 
-
- 提示： 
-
- 
- 链表中结点的数目为 sz 
- 1 <= sz <= 30 
- 0 <= Node.val <= 100 
- 1 <= n <= sz 
- 
-
- 
-
- 进阶：你能尝试使用一趟扫描实现吗？ 
-
- Related Topics 链表 双指针 👍 2893 👎 0
-
-*/
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * <p>
+ * Related Topics 链表 双指针 👍 2893 👎 0
+ */
 package editor.cn;
+
 //java:删除链表的倒数第 N 个结点
-class P19RemoveNthNodeFromEndOfList{
-    public static void main(String[] args){
+class P19RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
         Solution solution = new P19RemoveNthNodeFromEndOfList().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        // 创建虚拟头节点
-        ListNode dummy = new ListNode();
-        // 让虚拟头节点指向原链表的头节点head
-        dummy.next = head;
-        // 定义快慢指针
-        ListNode fast = dummy;
-        ListNode slow = dummy;
-        // 快指针先走n+1步，目的是为了在快指针指向链表结尾null时，慢指针指向要删除的倒数节点的前一个节点，这样才好做节点的删除
-        for (int i = 0; i <= n; i++) {
-            fast = fast.next;
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            // 创建虚拟头节点
+            ListNode dummy = new ListNode();
+            // 让虚拟头节点指向原链表的头节点head
+            dummy.next = head;
+            // 定义快慢指针
+            ListNode fast = dummy;
+            ListNode slow = dummy;
+            // 快指针先走n+1步，目的是为了在快指针指向链表结尾null时，慢指针指向要删除的倒数节点的前一个节点，这样才好做节点的删除
+            for (int i = 0; i <= n; i++) {
+                fast = fast.next;
+            }
+            // 快慢指针一起走，直到快指针到达链表结尾null
+            while (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return dummy.next;
         }
-        // 快慢指针一起走，直到快指针到达链表结尾null
-        while (fast != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
-        return dummy.next;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    //leetcode submit region end(Prohibit modification and deletion)
 // 单向链表定义
-public class ListNode {
-    int val;
-    ListNode next;
+    public class ListNode {
+        int val;
+        ListNode next;
 
-    // 无参数构造函数
-    ListNode() {
-    }
+        // 无参数构造函数
+        ListNode() {
+        }
 
-    // 节点中存储的值
-    ListNode(int val) {
-        this.val = val;
-    }
+        // 节点中存储的值
+        ListNode(int val) {
+            this.val = val;
+        }
 
-    // 有参构造函数
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        // 有参构造函数
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
-}
 
 }
